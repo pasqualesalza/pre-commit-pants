c0cb5d0d113725e2b63009e0a0762b5ac65b16ba
Pasquale Salza <pasquale.salza@gmail.com>
2023-12-29 15:46:59 +0100

    Update version retrieval in __init__.py

--- pre_commit_pants/__init__.py
+++ pre_commit_pants/__init__.py
@@ -1,7 +1,4 @@
-import toml
+from importlib.metadata import version
 
-# Read the version from pyproject.toml.
-with open("pyproject.toml", "r") as f:
-    pyproject_toml = toml.load(f)
-    __app_name__ = pyproject_toml["tool"]["poetry"]["name"]
-    __version__ = pyproject_toml["tool"]["poetry"]["version"]
+__app_name__ = "pre-commit-pants"
+__version__ = version(__app_name__)
